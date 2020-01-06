9f9d48a1d5935aefc1194304e6c3217d06b216c0
rixiobarrios <rixiobarrios.consulado@gmail.com>
2020-01-06 17:39:45 -0600

    Updated HTML, Javascript files, added fonts/images

--- README.md
+++ README.md
@@ -1,51 +1,53 @@
-#Project 1: Rock Paper Scissors Game
+# Project 1: Rock Paper Scissors Game
 
 This is a game project made in HTML, CSS and Javascript. Its a very simple game of Rock, paper scissors. I made this project as part of my first project assignment for my coding bootcamp school (General Assembly).
 
-Here is a Gif of the program in action:
+Here is a GIF of the game in action:
 ![Alt text](relative/path/to/img.jpg?raw=true 'Title')
 ![Example Image](./images/example.gif)
 
-##Target Audience
+## Target Audience
 
 This game is targeting kids from 3 to 100 years old.
 
-##Instructions
+## Instructions
 
 Chose one of three choices to play against the computer. Once you have chosen, the computer would also make a choice and a win, a lose or a draw message would be posted for each party.
 
-##Rules
+## Rules
 
+-   Rock smashes scissors = Rock wins
 -   Paper covers Rock = Paper wins
 -   Scissors cut paper = Scissors wins
--   Rock smashes scissors = Rock wins
 -   Rock and rock = Draw
 -   Paper and paper = Draw
 -   Scissor and Scissors = Draw
 
-##Features
+## Features
 
 -   Scoreboard tops after 10 wins
 -   Reset buttons restart the game
 -   Three buttons picks the three options
 -   2 graphic representations change to reflect chosen game object
 
-##Bugs
+## Bugs
 
 none yet
 
-##Future additions
+## Future additions
 
 Animations for computer's choice. Mobile phone layout. Available version in Spanish Unlockable/achievements graphics. New UI for chosen game object.
 
-##Attributions
+## Attributions
 
-Student logo for this file was created using freelogodesign.org/
+Student logo for this file was created using freelogodesign.org
 
-Wireframes made using wireframe.cc
+Wireframes were made using wireframe.cc
 
 Graphics sourced for hands from Esquire article https://www.esquireme.com/content/20945-how-to-win-at-rock-paper-scissors
 
+Fonts provided by dafont.com
+
 Background vector created by freepik.com
 
-##Thanks
+## Thanks
--- script.js
+++ script.js
@@ -1,4 +1,81 @@
-// ### PROJECT 1 ###
+// PROJECT 1
 
+//start the game/button
+// let startGame()
+//user choses from three options/buttons
+//<---- MDN syntax ---->
+const rockButton = document.getElementById('rock');
+rockButton.addEventListener('click', function(event) {
+    console.log('The rock button has been clicked');
+});
 
-a bunch of javascript ConvolverNode, right here!
+const paperButton = document.getElementById('paper');
+paperButton.addEventListener('click', function(event) {
+    console.log('The paper button has been clicked');
+});
+
+const scissorsButton = document.getElementById('scissors');
+scissorsButton.addEventListener('click', function(event) {
+    console.log('The scissors button has been clicked');
+});
+
+//1. need to add event listeners
+
+//2. define options with a class name for each element
+
+//3. store the user's choice in a variable
+// let playerChoice =
+
+//4. tell the computer that is its turn
+//     let computerChoice =
+//         //computer choses
+//         //1. create an array with the three elements to chose from
+//     let optionsToSelect = ['rock', 'paper', 'scissors'];
+// //2. generate a random response using math.random(google get a random element from an array)
+// //<---- geeksforgeeks syntax ---->
+result = [4];
+arr = [1, 2, 3];
+
+function computerChoice() {
+    result.innerText = arr[Math.floor(Math.random() * arr.length)];
+    console.log(result.innerText);
+}
+
+computerChoice();
+//3. store computers choice in a variable
+// computerChoice = [];
+//we compare the choices
+//1. possible use a switch statement or if statement
+//<---- w3schools syntax ---->
+// let(playerChoice) {
+//     switch (answer) {
+//         case 'rock' === 'scissors':
+//         case 'paper' === 'rock':
+//         case 'scissors' === 'paper':
+//             answer = 'You Win!';
+//             break;
+//         case 'rock' === 'paper':
+//         case 'paper' === 'scissors':
+//         case 'scissors' === 'rock':
+//             answer = 'You Lose';
+//             break;
+//         case 'rock' === 'rock':
+//         case 'paper' === 'paper':
+//         case 'scissors' === 'scissors':
+//             answer = 'Its a Draw';
+//             break;
+//     }
+// }
+//<---- MDN syntax ---->
+// function choice() {
+//     if 'rock' === 'rock';
+//     return 'Its a draw';
+// } else {
+//     return 'You win!';
+// }
+//determine and declare winner
+//1. update the screen possible use innerText and
+// .innerText
+//increase score
+//1. possibly use the incrementor
+//i++
