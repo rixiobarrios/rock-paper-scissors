a3c062357bd0600b7be83072fa5684a07f5f5e15
rixiobarrios <rixiobarrios.consulado@gmail.com>
2020-01-06 21:47:30 -0600

    Updated README and Javascript files

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
-# Project 1: Rock Paper Scissors Game
+![Example Image](./img/log.png)
+
+# H1 Project 1: Rock Paper Scissors Game
 
 This is a game project made in HTML, CSS and Javascript. Its a very simple game of Rock, paper scissors. I made this project as part of my first project assignment for my coding bootcamp school (General Assembly).
 
 Here is a GIF of the game in action:
-![Alt text](relative/path/to/img.jpg?raw=true 'Title')
 ![Example Image](./images/example.gif)
 
-## Target Audience
+## H2 Target Audience
 
 This game is targeting kids from 3 to 100 years old.
 
-## Instructions
+## H2 Instructions
 
 Chose one of three choices to play against the computer. Once you have chosen, the computer would also make a choice and a win, a lose or a draw message would be posted for each party.
 
-## Rules
+## H2 Rules
 
 -   Rock smashes scissors = Rock wins
 -   Paper covers Rock = Paper wins
@@ -23,31 +24,35 @@ Chose one of three choices to play against the computer. Once you have chosen, t
 -   Paper and paper = Draw
 -   Scissor and Scissors = Draw
 
-## Features
+## H2 Features
 
 -   Scoreboard tops after 10 wins
 -   Reset buttons restart the game
 -   Three buttons picks the three options
 -   2 graphic representations change to reflect chosen game object
 
-## Bugs
+## H2 Bugs
 
 none yet
 
-## Future additions
+## H2 Future additions
 
-Animations for computer's choice. Mobile phone layout. Available version in Spanish Unlockable/achievements graphics. New UI for chosen game object.
+-   Animations
+-   Mobile phone layout
+-   Available version in Spanish
+-   Unlockable/achievements graphics
+-   New UI for chosen game object.
 
-## Attributions
+## H2 Attributions
 
-Student logo for this file was created using freelogodesign.org
+Personal branding logo for this file was created using [freelogodesign.org](https://www.freelogodesign.org/)
 
-Wireframes were made using wireframe.cc
+Wireframes were made using [wireframe.cc](https://wireframe.cc/)
 
-Graphics sourced for hands from Esquire article https://www.esquireme.com/content/20945-how-to-win-at-rock-paper-scissors
+Graphics sourced for hands from Esquire article [esquireme.com](https://www.esquireme.com/content/20945-how-to-win-at-rock-paper-scissors)
 
-Fonts provided by dafont.com
+Fonts provided by [dafont.com](https://www.dafont.com/)
 
-Background vector created by freepik.com
+Background vector created by [freepik.com](https://www.freepik.com/home)
 
-## Thanks
+## H2 Thanks
--- script.js
+++ script.js
@@ -2,29 +2,40 @@
 
 //start the game/button
 // let startGame()
+playerScore = 0;
+computerScore = 0;
 //user choses from three options/buttons
 //<---- MDN syntax ---->
+let playerChoice;
+
 const rockButton = document.getElementById('rock');
 rockButton.addEventListener('click', function(event) {
     console.log('The rock button has been clicked');
+    playerChoice = event.target.innerText;
+    console.log('playerChoice', playerChoice);
 });
 
 const paperButton = document.getElementById('paper');
 paperButton.addEventListener('click', function(event) {
     console.log('The paper button has been clicked');
+    playerChoice = event.target.innerText;
+    console.log('playerChoice', playerChoice);
 });
 
 const scissorsButton = document.getElementById('scissors');
 scissorsButton.addEventListener('click', function(event) {
     console.log('The scissors button has been clicked');
+    playerChoice = event.target.innerText;
+    console.log('playerChoice', playerChoice);
 });
-
 //1. need to add event listeners
 
 //2. define options with a class name for each element
 
-//3. store the user's choice in a variable
+//3. store the player's choice in a variable
 // let playerChoice =
+
+// let computerChoice =
 
 //4. tell the computer that is its turn
 //     let computerChoice =
@@ -33,8 +44,8 @@ scissorsButton.addEventListener('click', function(event) {
 //     let optionsToSelect = ['rock', 'paper', 'scissors'];
 // //2. generate a random response using math.random(google get a random element from an array)
 // //<---- geeksforgeeks syntax ---->
-result = [4];
-arr = [1, 2, 3];
+result = [];
+arr = ['rock', 'paper', 'scissors'];
 
 function computerChoice() {
     result.innerText = arr[Math.floor(Math.random() * arr.length)];
@@ -47,35 +58,66 @@ computerChoice();
 //we compare the choices
 //1. possible use a switch statement or if statement
 //<---- w3schools syntax ---->
-// let(playerChoice) {
-//     switch (answer) {
-//         case 'rock' === 'scissors':
-//         case 'paper' === 'rock':
-//         case 'scissors' === 'paper':
-//             answer = 'You Win!';
-//             break;
-//         case 'rock' === 'paper':
-//         case 'paper' === 'scissors':
-//         case 'scissors' === 'rock':
-//             answer = 'You Lose';
-//             break;
-//         case 'rock' === 'rock':
-//         case 'paper' === 'paper':
-//         case 'scissors' === 'scissors':
-//             answer = 'Its a Draw';
-//             break;
-//     }
+// switch (function()) {
+//     case 'rock' === 'scissors':
+//     case 'paper' === 'rock':
+//     case 'scissors' === 'paper':
+//         answer = 'You Win!';
+//         break;
+//     case 'rock' === 'paper':
+//     case 'paper' === 'scissors':
+//     case 'scissors' === 'rock':
+//         answer = 'You Lose';
+//         break;
+//     case 'rock' === 'rock':
+//     case 'paper' === 'paper':
+//     case 'scissors' === 'scissors':
+//         answer = 'Its a Draw';
+//         break;
 // }
+// console.log(computerChoices);
 //<---- MDN syntax ---->
-// function choice() {
-//     if 'rock' === 'rock';
-//     return 'Its a draw';
-// } else {
-//     return 'You win!';
-// }
+function calculateResults() {
+    if (playerChoice === 'Rock' && computerChoice === 'Paper') {
+        console.log('player wins');
+    } else if (playerChoice === 'Paper' && computerChoice === 'Rock') {
+        console.log('player wins');
+    } else if (playerChoice === 'Scissors' && computerChoice === 'Paper') {
+        console.log('player wins');
+    } else if (playerChoice === 'Rock' && computerChoice === 'Paper') {
+        console.log('computer wins');
+    } else if (playerChoice === 'Paper' && computerChoice === 'Scissors') {
+        console.log('computer wins');
+    } else if (playerChoice === 'Scissors' && computerChoice === 'Rock') {
+        console.log('computer wins');
+    } else if (playerChoice === 'Rock' && computerChoice === 'Rock') {
+        console.log('Its a draw');
+    } else if (playerChoice === 'Paper' && computerChoice === 'Paper') {
+        console.log('Its a draw');
+    } else if (playerChoice === 'Scissors' && computerChoice === 'Scissors') {
+        console.log('Its a draw');
+    }
+}
+
+calculateResults();
 //determine and declare winner
+
 //1. update the screen possible use innerText and
 // .innerText
 //increase score
 //1. possibly use the incrementor
 //i++
+//stop score
+// for (let counter = 1; counter < 6; counter++) {
+//     let userPrompt = prompt('Do you choose heads or tails?');
+//     if (userPrompt === 'heads') {
+//         console.log("You've chosen heads; the computer chooses tails.");
+//     } else if (userPrompt === 'tails') {
+//         console.log("You've chosen tails; the computer chooses heads.");
+//     } else {
+//         console.log(
+//             "You've chosen " + userPrompt + ', which is an invalid choice.'
+//         );
+//     }
+// }
+////////////
