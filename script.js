c8e3b928764bfc57cdf176e5a6e135dd0a33fbed
Qusai Fares <qusaifares@gmail.com>
2020-09-18 15:33:41 -0400

    fix: fix audio to play on game start

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
 // PROJECT 1 // ROCK PAPER SCISSORS GAME VERSION 1.0
 
 //start music
-function myStartMusic() {
-    let startMusic = document.getElementById('start-music');
-    startMusic.play();
-}
-myStartMusic();
-
-window.location.reload = function () {
-    myStartMusic();
-};
-// let myStartMusic = document.getElementById('start-music');
-// function play() {
-
-// }
-// myStartMusic.play()
+let startMusic = document.getElementById('start-music');
 
 //start the game/button/intro screen
 const playButton = document.querySelector('.intro button');
 const introScreen = document.querySelector('.intro');
 playButton.addEventListener('click', function () {
-    introScreen.style.display = 'none';
+  introScreen.style.display = 'none';
+  startMusic.play();
 });
 
 //initial score
@@ -30,12 +18,12 @@ let drawScore = 0;
 
 //reset scores
 function resetScores() {
-    playerScore = 0;
-    computerScore = 0;
-    drawScore = 0;
-    document.getElementById('player-score').innerText = playerScore;
-    document.getElementById('computer-score').innerText = computerScore;
-    document.getElementById('draw-score').innerText = drawScore;
+  playerScore = 0;
+  computerScore = 0;
+  drawScore = 0;
+  document.getElementById('player-score').innerText = playerScore;
+  document.getElementById('computer-score').innerText = computerScore;
+  document.getElementById('draw-score').innerText = drawScore;
 }
 
 // Game sounds
@@ -54,27 +42,27 @@ let winSound = new Audio((src = 'sounds/win.mp3'));
 const winButton = document.querySelector('.win button');
 const winScreen = document.querySelector('.win');
 winButton.addEventListener('click', function () {
-    winScreen.style.display = 'none';
-    resetScores();
-    resetHands();
-    resetMessage();
+  winScreen.style.display = 'none';
+  resetScores();
+  resetHands();
+  resetMessage();
 });
 
 const loseButton = document.querySelector('.lose button');
 const loseScreen = document.querySelector('.lose');
 loseButton.addEventListener('click', function () {
-    loseScreen.style.display = 'none';
-    resetScores();
-    resetHands();
-    resetMessage();
+  loseScreen.style.display = 'none';
+  resetScores();
+  resetHands();
+  resetMessage();
 });
 const drawButton = document.querySelector('.draw button');
 const drawScreen = document.querySelector('.draw');
 drawButton.addEventListener('click', function () {
-    drawScreen.style.display = 'none';
-    resetScores();
-    resetHands();
-    resetMessage();
+  drawScreen.style.display = 'none';
+  resetScores();
+  resetHands();
+  resetMessage();
 });
 
 //player and computer objects/hands
@@ -83,13 +71,13 @@ const computerHand = document.querySelector('#computer-hand');
 
 //reset hands
 function resetHands() {
-    playerHand.src = './img/rock_left.png';
-    computerHand.src = './img/rock.png';
+  playerHand.src = './img/rock_left.png';
+  computerHand.src = './img/rock.png';
 }
 
 //reset announcement
 function resetMessage() {
-    document.getElementById('announcement').innerText = 'READY TO PLAY?';
+  document.getElementById('announcement').innerText = 'READY TO PLAY?';
 }
 
 //player chooses from three options/buttons
@@ -111,16 +99,16 @@ scissorsButton.addEventListener('click', handleClick);
 let playerChoice;
 //<---- w3schools ---->
 function handleClick(event) {
-    playerHand.src = './img/rock_left.png';
-    computerHand.src = './img/rock.png';
-    playerChoice = event.target.innerText;
-    playerHand.classList.add('shake');
-    computerHand.classList.add('shake');
-    setTimeout(function () {
-        playerHand.classList.remove('shake');
-        computerHand.classList.remove('shake');
-        computerPlay();
-    }, 1500);
+  playerHand.src = './img/rock_left.png';
+  computerHand.src = './img/rock.png';
+  playerChoice = event.target.innerText;
+  playerHand.classList.add('shake');
+  computerHand.classList.add('shake');
+  setTimeout(function () {
+    playerHand.classList.remove('shake');
+    computerHand.classList.remove('shake');
+    computerPlay();
+  }, 1500);
 }
 
 //create an array with the three elements to chose from
@@ -131,8 +119,8 @@ choices = ['Rock', 'Paper', 'Scissors'];
 let computerChoice;
 //<---- geeksforgeeks syntax ---->
 function computerPlay() {
-    computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    calculateResults();
+  computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  calculateResults();
 }
 
 //we compare the choices
@@ -141,102 +129,102 @@ function computerPlay() {
 //<---- w3schools syntax ---->
 //<---- MDN syntax ---->
 function calculateResults() {
-    if (playerChoice === 'Rock' && computerChoice === 'Paper') {
-        document.getElementById('announcement').innerText = 'YOU LOSE';
-        computerScore += 1;
-        badSound.play();
-    } else if (playerChoice === 'Paper' && computerChoice === 'Rock') {
-        document.getElementById('announcement').innerText = 'YOU WIN!!!';
-        playerScore += 1;
-        goodSound.play();
-    } else if (playerChoice === 'Scissors' && computerChoice === 'Paper') {
-        document.getElementById('announcement').innerText = 'YOU WIN!!!';
-        playerScore += 1;
-        goodSound.play();
-    } else if (playerChoice === 'Paper' && computerChoice === 'Scissors') {
-        document.getElementById('announcement').innerText = 'YOU LOSE';
-        computerScore += 1;
-        badSound.play();
-    } else if (playerChoice === 'Scissors' && computerChoice === 'Rock') {
-        document.getElementById('announcement').innerText = 'YOU LOSE';
-        computerScore += 1;
-        badSound.play();
-    } else if (playerChoice === 'Rock' && computerChoice === 'Scissors') {
-        document.getElementById('announcement').innerText = 'YOU WIN!!!';
-        playerScore += 1;
-        goodSound.play();
-    } else if (playerChoice === 'Rock' && computerChoice === 'Rock') {
-        document.getElementById('announcement').innerText = "IT'S A DRAW";
-        drawScore += 1;
-        drawSound.play();
-    } else if (playerChoice === 'Paper' && computerChoice === 'Paper') {
-        document.getElementById('announcement').innerText = "IT'S A DRAW";
-        drawScore += 1;
-        drawSound.play();
-    } else if (playerChoice === 'Scissors' && computerChoice === 'Scissors') {
-        document.getElementById('announcement').innerText = "IT'S A DRAW";
-        drawScore += 1;
-        drawSound.play();
-    }
+  if (playerChoice === 'Rock' && computerChoice === 'Paper') {
+    document.getElementById('announcement').innerText = 'YOU LOSE';
+    computerScore += 1;
+    badSound.play();
+  } else if (playerChoice === 'Paper' && computerChoice === 'Rock') {
+    document.getElementById('announcement').innerText = 'YOU WIN!!!';
+    playerScore += 1;
+    goodSound.play();
+  } else if (playerChoice === 'Scissors' && computerChoice === 'Paper') {
+    document.getElementById('announcement').innerText = 'YOU WIN!!!';
+    playerScore += 1;
+    goodSound.play();
+  } else if (playerChoice === 'Paper' && computerChoice === 'Scissors') {
+    document.getElementById('announcement').innerText = 'YOU LOSE';
+    computerScore += 1;
+    badSound.play();
+  } else if (playerChoice === 'Scissors' && computerChoice === 'Rock') {
+    document.getElementById('announcement').innerText = 'YOU LOSE';
+    computerScore += 1;
+    badSound.play();
+  } else if (playerChoice === 'Rock' && computerChoice === 'Scissors') {
+    document.getElementById('announcement').innerText = 'YOU WIN!!!';
+    playerScore += 1;
+    goodSound.play();
+  } else if (playerChoice === 'Rock' && computerChoice === 'Rock') {
+    document.getElementById('announcement').innerText = "IT'S A DRAW";
+    drawScore += 1;
+    drawSound.play();
+  } else if (playerChoice === 'Paper' && computerChoice === 'Paper') {
+    document.getElementById('announcement').innerText = "IT'S A DRAW";
+    drawScore += 1;
+    drawSound.play();
+  } else if (playerChoice === 'Scissors' && computerChoice === 'Scissors') {
+    document.getElementById('announcement').innerText = "IT'S A DRAW";
+    drawScore += 1;
+    drawSound.play();
+  }
 
-    // announcement animation <-------  future feature/work in progress
-    // function bounceInMessage() {
-    //     const announcement = document.getElementById('announcement');
-    //     announcement.classList.add('bounce');
-    // }
+  // announcement animation <-------  future feature/work in progress
+  // function bounceInMessage() {
+  //     const announcement = document.getElementById('announcement');
+  //     announcement.classList.add('bounce');
+  // }
 
-    // function bounceInMessage() {
-    //     const announcement = document.getElementById('announcement');
-    //     announcement.addEventListener('bounce');
-    // }
+  // function bounceInMessage() {
+  //     const announcement = document.getElementById('announcement');
+  //     announcement.addEventListener('bounce');
+  // }
 
-    //image representation for player and computer turn choices
-    replacePlayerImage();
-    replaceComputerImage();
-    document.getElementById('draw-score').innerText = drawScore;
-    document.getElementById('player-score').innerText = playerScore;
-    document.getElementById('computer-score').innerText = computerScore;
-    displayResults();
+  //image representation for player and computer turn choices
+  replacePlayerImage();
+  replaceComputerImage();
+  document.getElementById('draw-score').innerText = drawScore;
+  document.getElementById('player-score').innerText = playerScore;
+  document.getElementById('computer-score').innerText = computerScore;
+  displayResults();
 }
 
 //stop score
 //declare results
 //<---- teamtreehouse ---->
 function displayResults() {
-    if (playerScore === 3) {
-        winScreen.style.display = 'flex';
-        goodSound.pause();
-        winSound.play();
-    } else if (computerScore === 3) {
-        loseScreen.style.display = 'flex';
-        badSound.pause();
-        loserSound.play();
-    } else if (drawScore === 3) {
-        drawScreen.style.display = 'flex';
-        drawSound.pause();
-        tieSound.play();
-    }
+  if (playerScore === 3) {
+    winScreen.style.display = 'flex';
+    goodSound.pause();
+    winSound.play();
+  } else if (computerScore === 3) {
+    loseScreen.style.display = 'flex';
+    badSound.pause();
+    loserSound.play();
+  } else if (drawScore === 3) {
+    drawScreen.style.display = 'flex';
+    drawSound.pause();
+    tieSound.play();
+  }
 }
 // clear score
 //change images per turn
 //<---- MDN ---->
 function replacePlayerImage() {
-    if (playerChoice === 'Rock') {
-        playerHand.src = './img/rock_left.png';
-    } else if (playerChoice === 'Paper') {
-        playerHand.src = './img/paper_left.png';
-    } else if (playerChoice === 'Scissors') {
-        playerHand.src = './img/scissors_left.png';
-    }
+  if (playerChoice === 'Rock') {
+    playerHand.src = './img/rock_left.png';
+  } else if (playerChoice === 'Paper') {
+    playerHand.src = './img/paper_left.png';
+  } else if (playerChoice === 'Scissors') {
+    playerHand.src = './img/scissors_left.png';
+  }
 }
 
 function replaceComputerImage() {
-    if (computerChoice === 'Rock') {
-        computerHand.src = './img/rock.png';
-    } else if (computerChoice === 'Paper') {
-        computerHand.src = './img/paper.png';
-    } else if (computerChoice === 'Scissors') {
-        computerHand.src = './img/scissors.png';
-    }
+  if (computerChoice === 'Rock') {
+    computerHand.src = './img/rock.png';
+  } else if (computerChoice === 'Paper') {
+    computerHand.src = './img/paper.png';
+  } else if (computerChoice === 'Scissors') {
+    computerHand.src = './img/scissors.png';
+  }
 }
 // add instructions
